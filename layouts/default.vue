d9fbc19d19d22686023c4c05fe32d129bc96fc0e
tankson <57566808+tankson@users.noreply.github.com>
2020-02-16 14:28:46 +0800

    项目公共页脚组件完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,16 +4,22 @@
       <Header/>
     <!-- 内容占位组件 -->
     <nuxt />
+     <!-- 页脚组件 -->
+     <Footer/>
   </div>
 </template>
 
 <script>
 // 导入头部组件
 import Header from "@/components/header";
+// 导入页脚组件
+import Footer from "@/components/footer";
+
 export default {
   // 注册组件
    components: {
-    Header
+    Header,
+    Footer
   }
 
 
